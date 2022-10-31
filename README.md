18a71aa89c1eebab93abd4888d8d95f487718139
Lasse Lang <lang.lasse@gmx.de>
2022-10-31 12:20:27 +0100

    Added first files

---
+++ README.md
@@ -0,0 +1,3 @@
+# blog template
+
+# ❗️ Work in progress
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.listen(80, () => {
+  console.log("Server started on port 80")
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html")
+})
+
+app.get("/style.css", (req, res) => {
+  res.sendFile(__dirname + "/client/style.css")
+})
+
+app.get("/images/titleImage.png", (req, res) => {
+  res.sendFile(__dirname + "/client/images/titleImage.png")
+})
